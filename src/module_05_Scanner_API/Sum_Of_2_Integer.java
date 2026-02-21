7a96892a22de9eff36a3ce1bc8d18a843caedbca
RaunakS999 <ellmc2raunak@gmail.com>
2026-02-21 10:18:14 +0530

    docs: add question

---
+++ src/module_05_Scanner_API/Sum_Of_2_Integer.java
@@ -0,0 +1,17 @@
+package module_05_Scanner_API;
+/**
+ * MODULE: Fundamental Programming
+ * QUESTION: Sum of Two Integers
+ * * DESCRIPTION:
+ * Reads two integers from separate lines and prints their sum.
+ * * INPUT FORMAT:
+ * - Two integers 'a' and 'b' (-1000 <= a, b <= 1000).
+ * * OUTPUT FORMAT:
+ * - A single integer representing (a + b).
+ * * EXAMPLES:
+ * 1. Input: 10, 20  -> Output: 30
+ * 2. Input: -16, 10 -> Output: -6
+ */
+public class Sum_Of_2_Integer {
+
+}
